d6c52bac6c302ab862ad0082dd259f98863c77e0
Jose David Giraldo Mosquera <josedavidgiraldomosquera@David-Cali-MacBook-Pro.local>
2019-09-24 15:14:32 -0500

    1 and 0 being painting

--- progressive_background_camera_example.js
+++ progressive_background_camera_example.js
@@ -1,96 +1,8 @@
-/*****************************************************************************\
-********************************** V I D A ************************************
-*******************************************************************************
-
-  p5.vida 0.3.00a by Paweł Janicki, 2017-2019
-    https://tetoki.eu/vida | https://paweljanicki.jp
-
-*******************************************************************************
-
-  VIDA by Paweł Janicki is licensed under a Creative Commons
-  Attribution-ShareAlike 4.0 International License
-  (http://creativecommons.org/licenses/by-sa/4.0/). Based on a work at:
-  https://tetoki.eu.
-
-*******************************************************************************
-
-  VIDA is a simple library that adds camera (or video) based motion detection
-  and blob tracking functionality to p5js.
-
-  The library allows motion detection based on a static or progressive
-  background; defining rectangular zones in the monitored image, inside which
-  the occurrence of motion triggers the reaction of the program; detection of
-  moving objects ("blobs") with unique index, position, mass, rectangle,
-  approximated polygon.
-
-  The main guidelines of the library are to maintain the code in a compact
-  form, easy to modify, hack and rework.
-
-  VIDA is a part of the Tetoki! project (https://tetoki.eu) and is developed
-  thanks to substantial help and cooperation with the WRO Art Center
-  (https://wrocenter.pl) and HAT Research Center
-  (http://artandsciencestudies.com).
-
-  Notes:
-
-    [1] Limitations: of course, the use of the camera from within web browser
-    is subject to various restrictions mainly related to security settings (in
-    particular, browsers differ significantly in terms of enabling access to
-    the video camera for webpages (eg p5js sketches) loaded from local media or
-    from the network - in the last case it is also important if the connection
-    uses the HTTPS protocol [or HTTP]). Therefore, if there are problems with
-    access to the video camera from within a web browser, it is worth testing a
-    different one. During developement, for on-the-fly checks, VIDA is mainly
-    tested with Firefox, which by default allows you to access the video camera
-    from files loaded from local media. VIDA itself does not impose any
-    additional specific restrictions related to the type and parameters of the
-    camera - any video camera working with p5js should work with the library.
-    You can find valuable information on this topic at https://webrtc.org and
-    in the documentation of the web browser you use.
-    
-    [2] Also it is worth remembering that blob detection is rather expensive
-    computationally, so it's worth to stick to the lowest possible video
-    resolutions if you plan to run your programs on the hardware, the
-    performance you are not sure. The efficiency in processing video from a
-    video camera and video files should be similar.
-
-    [3] VIDA is using (with a few exceptions) normalized coords instead of
-    pixel-based. Thus, the coordinates of the active zones, the location of
-    detected moving objects (and some of their other parameters) are
-    represented by floating point numbers within the range from 0.0 to 1.0. The
-    use of normalized coordinates and parameters allows to manipulate the
-    resolution of the image being processed (eg from a video camera) without
-    having to change eg the position of active zones. analogously, data
-    describing moving objects is easier to use, if their values are not related
-    to any specific resolution expressed in pixels. Names of all normalized
-    parameters are preceded by the prefix "norm". The upper left corner of the
-    image has the coordinates [0.0, 0.0]. The bottom right corner of the image
-    has the coordinates [1.0, 1.0].
-
-                      [0.0, 0.0]
-                      +------------------------------|
-                      |              [0.5, 0.2]      |
-                      |              +               |
-                      |                              |
-                      |      [0.25, 0.5]             |
-                      |      +                       |
-                      |                              |
-                      |                   [0.7, 0.8] |
-                      |                   +          |
-                      |                              |
-                      |------------------------------+
-                                                     [1.0, 1.0]
-                                                     
-\*****************************************************************************/
-
 let myCapture, // camera
   myVida; // VIDA
 
 let points = [];
 
-/*
-  Here we are trying to get access to the camera.
-*/
 let captureWidth, captureHeight;
 let pixelsData;
 
@@ -115,38 +27,13 @@ function setup() {
   createCanvas(windowWidth, windowHeight); // we need some space...
   initCaptureDevice(); // and access to the camera
 
-  /*
-    VIDA stuff. One parameter - the current sketch - should be passed to the
-    class constructor (thanks to this you can use Vida e.g. in the instance
-    mode).
-  */
-  myVida = new Vida(this); // create the object
-  /*
-    Turn on the progressive background mode.
-  */
+
+  /*myVida = new Vida(this); // create the object
   myVida.progressiveBackgroundFlag = true;
-  /*
-    You may need a horizontal image flip when working with the video camera.
-    If you need a different kind of mirror, here are the possibilities:
-      [your vida object].MIRROR_NONE
-      [your vida object].MIRROR_VERTICAL
-      [your vida object].MIRROR_HORIZONTAL
-      [your vida object].MIRROR_BOTH
-    The default value is MIRROR_NONE.
-  */
   myVida.mirror = myVida.MIRROR_HORIZONTAL; // uncomment if needed
-  /*
-    The value of the feedback for the procedure that calculates the background
-    image in progressive mode. The value should be in the range from 0.0 to 1.0
-    (float). Typical values of this variable are in the range between ~0.9 and
-    ~0.98.
-  */
+ 
   myVida.imageFilterFeedback = 0.90;
-  /*
-    The value of the threshold for the procedure that calculates the threshold
-    image. The value should be in the range from 0.0 to 1.0 (float).
-  */
-  myVida.imageFilterThreshold = 0.10;
+  myVida.imageFilterThreshold = 0.10;*/
 
   frameRate(30); // set framerate
   pixelsDatan = (captureWidth * captureHeight) * 4;
@@ -157,30 +44,12 @@ function setup() {
 function draw() {
   if (myCapture !== null && myCapture !== undefined) { // safety first
     background(0);
-    /*
-      Call VIDA update function, to which we pass the current video frame as a
-      parameter. Usually this function is called in the draw loop (once per
-      repetition).
-    */
-    myVida.update(myCapture);
-    /*
-      Now we can display images: source video and subsequent stages of image
-      transformations made by VIDA.
-    */
-    /*image(myCapture, 0, 0);
-    image(myVida.backgroundImage, 320, 0);
-    image(myVida.differenceImage, 0, 240);*/
-    //image(myVida.thresholdImage, 0, 0);
 
-    readPixelsValue(myVida.thresholdImage);
+    //image(myCapture, 0, 0);
+    readPixelsValue(myCapture);
     //readPixelsValue(myCapture);
 
   } else {
-    /*
-      If there are problems with the capture device (it's a simple mechanism so
-      not every problem with the camera will be detected, but it's better than
-      nothing) we will change the background color to alarmistically red.
-    */
     background(255, 0, 0);
   }
 
@@ -205,8 +74,14 @@ function readPixelsValue(imageToRead) {
 
         noStroke();
         fill(r, g, b, a);
-        ellipse(x * 4, y * 4, 5, 5);
-        text('0',x*4,y*4);
+       //ellipse(x * 4, y * 4, 5, 5);
+        if(r<150 && g<100){
+          fill(0);
+          text('0',x*4,y*4);
+        }else{
+          text('1',x*4,y*4);
+        }
+       
       }
 
 
